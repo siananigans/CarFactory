c91bb0c01d756805d478183e0a290b489bfd6d7d
siananigans <sianlennon109@gmail.com>
2021-12-04 00:36:40 +0000

    Adding options menu and get order support

--- src/com/company/CarOrder.java
+++ src/com/company/CarOrder.java
@@ -1,26 +1,27 @@
 package com.company;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 
 class CarOrder {
     int carCount;
     String carMake;
     String carModel;
-    int orderDeadlineDays;
+    int daysToComplete;
     int orderNumber;
 
     public CarOrder(int carCount, String carMake, String carModel, int orderDeadlineDays, int orderNumber){
         this.carMake = carMake;
         this.carModel = carModel;
         this.carCount = carCount;
-
-        this.orderDeadlineDays = orderDeadlineDays;
+        this.daysToComplete = orderDeadlineDays;
         this.orderNumber = orderNumber;
     }
     public Car getOrderedCar(HashMap<String, ArrayList<Car>> cars) {
         ArrayList<Car> modelCars = cars.get(carMake);
+        System.out.println(modelCars);
         for (Car car : modelCars) {
-            if (car.make == carMake) {
+            if (Objects.equals(car.make, carMake)) {
                 return car;
             }
         }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,5 @@
 package com.company;
-import java.util.HashMap;
-import java.util.Scanner;
-import java.util.Random;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
 
@@ -13,41 +10,67 @@ public class Main {
 
         // Set up Assembly Factory
         AssemblyFactory assemblyFactory = new AssemblyFactory();
-
-        int order = 1;
+        HashMap<Integer, CarOrder> carOrders = new HashMap<>();
+        int orderNumber = 1;
         while(true) {
             // Read in car data
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter Make:");
-            String make = scanner.nextLine();
-            System.out.println("Enter Model:");
-            String model = scanner.nextLine();
-            System.out.println("Enter Count:");
-            int count = scanner.nextInt();
-            System.out.println("Enter due date:");
-            int deadlineDays = scanner.nextInt();
+            System.out.println("Welcome to our interactive Car Factory");
+            System.out.println("Options: \n 1. Create new order \n 2. Get existing order");
+            String option1 = scanner.nextLine();
+            if (Objects.equals(option1, "2")){
+                System.out.println("Enter Order Number:");
+                Integer queryOrderNumber = scanner.nextInt();
+                CarOrder carOrder = carOrders.get(queryOrderNumber);
+                if (carOrder != null) {
+                    System.out.println("Car Order Number " + orderNumber);
+                    System.out.println("Make: " + carOrder.carMake);
+                    System.out.println("Model: " + carOrder.carModel);
+                    System.out.println("Number of Cars: " + carOrder.carCount);
+                    System.out.println("Days Remaining: " + carOrder.daysToComplete);
+                }
+                else {
+                    System.out.println("Car order does not exist.");
+                }
+            }
+            else if (Objects.equals(option1, "1")){
 
-            // Create order
-            CarOrder carOrder = new CarOrder(count, make, model, deadlineDays, order);
-            order++;
+                System.out.println("Enter Make:");
+                String make = scanner.nextLine();
+                System.out.println("Enter Model:");
+                String model = scanner.nextLine();
+                System.out.println("Enter Count:");
+                int count = scanner.nextInt();
+                System.out.println("Enter due date:");
+                int deadlineDays = scanner.nextInt();
 
-            // Estimate completion time
-            Car orderedCar = carOrder.getOrderedCar(carStock);
-            int assemblyDays = assemblyFactory.estimateAssemblyTime(orderedCar.daysToAssemble * carOrder.carCount);
-            // Add 1 day to complete order
-            int daysToOrderComplete = assemblyDays + 1;
+                // Create order
+                CarOrder carOrder = new CarOrder(count, make, model, deadlineDays, orderNumber);
+                orderNumber++;
 
-            if (daysToOrderComplete < carOrder.orderDeadlineDays) {
-                System.out.println("Order Number: ");
-                System.out.println(carOrder.orderNumber);
-                System.out.println("Days until Order completion: ");
-                System.out.println(daysToOrderComplete);
+                // Estimate completion time
+                Car orderedCar = carOrder.getOrderedCar(carStock);
+                int assemblyDays = assemblyFactory.estimateAssemblyTime(orderedCar.daysToAssemble * carOrder.carCount);
+
+                // Add 1 day to complete order
+                int daysToOrderComplete = assemblyDays + 1;
+
+                if (daysToOrderComplete < carOrder.daysToComplete) {
+                    carOrders.put(carOrder.orderNumber, carOrder);
+                    System.out.println("Order Number: ");
+                    System.out.println(carOrder.orderNumber);
+                    System.out.println("Days until Order completion: ");
+                    System.out.println(daysToOrderComplete);
+                } else {
+                    System.out.println("Order cannot be processed in the desired time slot.");
+                    System.out.println("Estimated time to complete: ");
+                    System.out.println(daysToOrderComplete);
+                }
             }
             else {
-                System.out.println("Order cannot be processed in the desired time slot.");
-                System.out.println("Estimated time to complete: ");
-                System.out.println(daysToOrderComplete);
+                System.out.println("Invalid option, please choose a valid number input.");
             }
+            System.out.println();
         }
     }
 
@@ -71,7 +94,7 @@ public class Main {
             ArrayList<Car> makeCars = new ArrayList<>(); // Create an ArrayList object
             for (String s : models) {
                 int countInStock = getRandomNumber(0, 100000);
-                int daysToAssemble = getRandomNumber(0, 20);
+                int daysToAssemble = getRandomNumber(0, 10);
                 Car tmpCar = new Car(countInStock, i, make, s, daysToAssemble);
                 makeCars.add(tmpCar);
             }
